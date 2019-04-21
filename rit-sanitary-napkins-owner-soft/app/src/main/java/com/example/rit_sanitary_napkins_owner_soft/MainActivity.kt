3437ca9cb9ec1cf77acf9ce659054755b9550e89
kamal johnson <kamaljohnson12345@gmail.com>
2019-04-21 21:33:23 +0530

    adding owner app file

---
+++ rit-sanitary-napkins-owner-soft/app/src/main/java/com/example/rit_sanitary_napkins_owner_soft/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.rit_sanitary_napkins_owner_soft
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
