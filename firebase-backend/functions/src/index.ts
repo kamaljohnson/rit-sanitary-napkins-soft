1d52d837df9c57b9379f2d0e49fdc23b1a8815d7
kamal johnson <kamaljohnson12345@gmail.com>
2019-04-22 18:37:58 +0530

    updating cloud funcitons

--- firebase-backend/functions/src/index.ts
+++ firebase-backend/functions/src/index.ts
@@ -1,33 +1,53 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import { DataSnapshot } from 'firebase-functions/lib/providers/database';
+
 admin.initializeApp()
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
+DataSnapshot
+export const onUserDateTrigger = functions.database
+.ref('/users/{uid}')
+.onUpdate(async (change, context) => {
 
-export const onMachineScanTrigger = functions.database
-.ref('/users/{uid}/mid')
-.onUpdate((snapshot, context) => {
-    const uid = context.params.uid
-    const mid = snapshot.after.val().toString()
+    const before = change.before
+    const after = change.after
 
-    var machine_status:number = getMachineItemCost(mid)
-    console.log(`the cost of the item from the machine ${mid} is ${machine_status} Rs`)
+    if(before.val() === after.val()) {
+        return null
+    }
+    
+    const mid_before = before.child('mid').val()
+    const pin_before = before.child('pin').val()
+    const ctcost_before = before.child('ctcost').val()
+    const wallet_before = before.child('wallet').val()
+
+    const mid_after = after.child('mid').val()
+    const pin_after = after.child('pin').val()
+    const ctcost_after = after.child('ctcost').val()
+    const wallet_after = after.child('wallet').val()
+
+    if(mid_before !== mid_after){
+
+        const data = admin.database().ref('/machines/{mid}').once('value')
+        console.log(data)
+
+    }else if(pin_before !== pin_after) {
+
+        return after.ref.update({pin:"pin was changed"})
+
+    }else if(ctcost_after !== ctcost_before) {
+
+        return after.ref.update({ctcost:"ctcost was changed"})
+        
+    }else if(wallet_after !== wallet_before) {
+
+        return after.ref.update({wallet:"wallet was changed"})
+        
+    }
+    return null
 })
 
-function getMachineItemCost(mid:number) {
-    const db = admin.database().ref('/machines');
-    
-    const query = db.orderByChild('mid').equalTo(mid.toString()).limitToFirst(1);
 
-    if(query == null) {
-        return(0)
-    }    
-    else {
-        var cost:number = 10
-        //TODO: get the cost of the item from the database
-        return(cost)
-    }
-
-}
+//------------------Helper functions-------------------
---
+++ rit-sanitary-napkins-owner-soft/app/src/main/java/com/example/rit_sanitary_napkins_owner_soft/QRCodeScannerActivity.kt
@@ -0,0 +1,93 @@
+package com.example.rit_sanitary_napkins_owner_soft
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+import android.widget.TextView
+import android.widget.Toast
+import com.google.android.gms.vision.CameraSource
+import com.google.android.gms.vision.Detector
+import com.google.android.gms.vision.barcode.Barcode
+import com.google.android.gms.vision.barcode.BarcodeDetector
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+
+class QRCodeScannerActivity : AppCompatActivity() {
+
+    private lateinit var svBarcode: SurfaceView
+    private lateinit var tvBarcode: TextView
+
+    private lateinit var detector: BarcodeDetector
+    private lateinit var cameraSource: CameraSource
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_qrcode_scanner)
+
+        svBarcode = findViewById<SurfaceView>(R.id.sv_barcode)
+        tvBarcode = findViewById<TextView>(R.id.tv_barcode)
+
+        detector = BarcodeDetector.Builder(this).setBarcodeFormats(Barcode.QR_CODE).build()
+        detector.setProcessor(object : Detector.Processor<Barcode>{
+            override fun release() {
+
+            }
+
+            override fun receiveDetections(detections: Detector.Detections<Barcode>?) {
+                var barcodes = detections?.detectedItems
+                if(barcodes!!.size()>0){
+                    tvBarcode.post {
+                        tvBarcode.text = barcodes.valueAt(0).displayValue
+                        val uid = FirebaseAuth.getInstance().uid
+                        val ref = FirebaseDatabase.getInstance().getReference("users/$uid/mid")
+                        ref.setValue(tvBarcode.text.toString())
+                    }
+                }
+            }
+
+        })
+
+        cameraSource = CameraSource.Builder(this, detector).setRequestedPreviewSize(1024, 768)
+            .setRequestedFps(25F).setAutoFocusEnabled(true).build()
+        svBarcode.holder.addCallback(object : SurfaceHolder.Callback2 {
+            override fun surfaceRedrawNeeded(holder: SurfaceHolder?) {}
+            override fun surfaceChanged(holder: SurfaceHolder?, format: Int, width: Int, height: Int) {}
+            override fun surfaceDestroyed(holder: SurfaceHolder?) {
+                cameraSource.stop()
+            }
+
+            override fun surfaceCreated(holder: SurfaceHolder?) {
+                if(ContextCompat.checkSelfPermission(this@QRCodeScannerActivity,
+                        Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
+                    cameraSource.start(holder)
+                else ActivityCompat.requestPermissions(this@QRCodeScannerActivity,
+                    arrayOf(Manifest.permission.CAMERA), 123)
+            }
+
+        })
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == 123){
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                cameraSource.start(svBarcode.holder)
+            else Toast.makeText(this, "Scanner won't work without permission.", Toast.LENGTH_SHORT)
+                .show()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        detector.release()
+        cameraSource.stop()
+        cameraSource.release()
+    }
+}
